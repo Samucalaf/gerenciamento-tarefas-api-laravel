13a373ee40b3f5a37641f5366c66dcb524384fdc
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-07-24 14:04:06 -0300

    style: - Formatação de codigo

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,17 @@
+<?php
 
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/cadastro', function(){
+    //vai salvar usuarios na tabela usuarios
+});
+
+Route::post('/login', function(){
+    return response()->json(['mensagem' => 'Olá']);
+
+    //vai fazer uma pesquisa na tabela para realizar o login 
+});
 
 
