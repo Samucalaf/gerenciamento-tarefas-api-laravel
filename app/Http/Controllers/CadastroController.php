965c4fa9616e6017f481f0d6d4ed02996bc912f4
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-07-24 20:50:47 -0300

    feat: - implementação do cadastro do usuario com validações

--- app/Http/Controllers/CadastroController.php
+++ app/Http/Controllers/CadastroController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use ILLuminate\Support\Facades\DB;
 
 class CadastroController extends Controller
 {
-    //
+    public function cadastroUsuario(Request $request){
+        $nome = $request->input('nome');
+        $email = $request->input('email');
+        $senha = $request->input('senha');
+
+        $validate = $request->validate([
+            'nome' => 'required|string|max:255',
+            'email' => 'required|email|unique',
+            'senha' => 'nullable|string|min:18'
+        ]);
+
+        DB::insert('INSERT INTO usuarios (name, email, senha) VALUES (?, ?, ?)', [
+            $validate['nome'],
+            $validate['email'],
+            bcrypt($validate['senha']),
+        ]);
+}
 }
