d0286903ab2d212a5da092ef87d0ab2ead0c85c2
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-07-25 00:44:30 -0300

    fix: - Correção de bug

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,17 @@
 <?php
 
-
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CadastroController;
+
+Route::get('/teste', function(){
+    return 'OLá';
+});
 
 Route::post('/cadastro', [CadastroController::class, 'cadastroUsuario'])
     ->middleware('verifica.dados');
 
 
 
+Route::post('/login', [AuthController::class, 'login']);
