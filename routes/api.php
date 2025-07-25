e88ba1e6c114dbfc985454058b64b28a5b7a6fa2
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-07-24 21:47:16 -0300

    feat: - endpoint para o cadastro do usuario

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\CadastroController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/cadastro', function(){
-    //vai salvar usuarios na tabela usuarios
-});
+Route::post('/cadastro', [CadastroController::class, 'store'])->middleware('verifica.dados');
 
-Route::post('/login', function(){
-    return response()->json(['mensagem' => 'Olá']);
 
-    //vai fazer uma pesquisa na tabela para realizar o login 
-});
 
 
