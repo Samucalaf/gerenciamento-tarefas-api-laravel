3844093185b32d994221016e6b0e22fb6550d04e
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-07-25 22:08:13 -0300

    style: - Formatação de código

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,14 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CadastroController;
+use App\Http\Middleware\EnsureTokenIsValid;
 
-Route::get('/teste', function(){
+Route::get('/teste', function () {
     return 'OLá';
 });
 
 Route::post('/cadastro', [CadastroController::class, 'cadastroUsuario'])
-    ->middleware('verifica.dados');
-
+        ->middleware(EnsureTokenIsValid::class);
 
 
 Route::post('/login', [AuthController::class, 'login']);
