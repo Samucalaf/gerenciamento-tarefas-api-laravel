4e78b24d3677797d4c3ab1cb7ccf2447d357f583
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-07-27 23:06:57 -0300

    feat: - Nova funcionalidade: Metodo setPassword  adicionado dentro do model para transformar a senha em hash automaticamente

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\Contracts\HasApiTokens;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -45,5 +46,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function setPassowordAttribute($valor){
+        $this->attributes['password'] = Hash::make($valor);
     }
 }
