0630656afbc5155145762a8b0c499bd8f8268148
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-07-28 00:31:34 -0300

    feat: - Nova funcionalidade: Token é gerado no login  do user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,24 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    
-    public function login(Request $request){
-        
-        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])){
-            return response()->json(['mensagem' => 'Login realizado com sucesso']);
+
+    public function login(Request $request)
+    {
+
+        $validate = $request->validate([
+            'email' => 'required|email|',
+            'password' => 'required|string'
+        ]);
+
+
+        if (Auth::attempt($validate)) {
+
+            $user = User::where('email', $validate['email'])->first();
+
+            $token = $user->createToken('api-token')->plainTextToken;
+
+            return response()->json(['mensagem' => 'Login realizado com sucesso', 'token' => $token]);
         }
 
         return response()->json(['erro' => 'Credenciais invalidas'], 401);
     }
 
-    public function logout(){
-
-    }
+    public function logout() {}
 
 
 
