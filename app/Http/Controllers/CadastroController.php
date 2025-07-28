ea10b4004996fb636a0d246a7ecd355cfb7acc9f
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-07-27 23:10:28 -0300

    refactor: - Refatoração: validação melhorada com o create

--- app/Http/Controllers/CadastroController.php
+++ app/Http/Controllers/CadastroController.php
@@ -2,28 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class CadastroController extends Controller
 {
-    public function cadastroUsuario(Request $request){
-        $nome = $request->input('nome');
-        $email = $request->input('email');
-        $senha = $request->input('senha');
+    public function cadastroUsuario(Request $request)
+    {
 
         $validate = $request->validate([
-            'nome' => 'required|string|max:255',
+            'name' => 'required|string|max:255',
             'email' => 'required|email|unique:usuarios,email',
-            'senha' => 'nullable|string'
+            'password' => 'nullable|string'
         ]);
 
-        DB::insert('INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)', [
-            $validate['nome'],
-            $validate['email'],
-            bcrypt($validate['senha']),
+        $user = User::create($validate);
+
+        return response()->json([
+            'mensagem' => 'Usuário cadastrado com sucesso!',
+            'usuario' => $user
         ]);
-        return response()->json(['mensagem' => 'Usuário cadastrado com sucesso!']);
-}
+    }
 }
