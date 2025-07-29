6e1a76b9527966527b7af2cc45264e8cdd50d53b
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-07-29 15:25:00 -0300

    style: - Formatação de código adicionei o relacionamento com o usuario

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -11,4 +11,8 @@ class Task extends Model
     public function category(){
         return $this->belongsTo(Category::class);
     }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
