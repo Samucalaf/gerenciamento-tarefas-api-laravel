ae38326d3014263e408d6b1c973d3f5e4adf0f50
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-07-31 23:35:27 -0300

    feat: - Nova funcionalidade desenvolvimento do crud, index, store show implementados

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $category = Category::all();
+        return response()->json($category);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $valideted = $request->validate([
+            'name' => 'required|string|max:100'
+        ]);
+
+        $category = Category::create($valideted);
+        return response()->json($category, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $category = Category::findOrFail($id);
+        return response()->json($category);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $category = Category::findOrFail($id);
+
+        $validated = $request->validate([
+            'name' => 'required|string|max:100'
+        ]);
+
+        
+        return response()->json($validated);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $category = Category::findOrFail($id);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,15 +4,20 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CadastroController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Middleware\EnsureTokenIsValid;
 
 
 Route::prefix('auth')->group(function () {
         Route::post('/register', [AuthController::class, 'register']);
-
         Route::post('/login', [AuthController::class, 'login']);
         Route::post('/logout', [AuthController::class, 'logout']);
 });
+
+Route::middleware('auth:api')->group(function(){
+        Route::apiResource('category', CategoryController::class);
+});
+
 
 Route::get('/user', function (Request $request) {
         return $request->user();
