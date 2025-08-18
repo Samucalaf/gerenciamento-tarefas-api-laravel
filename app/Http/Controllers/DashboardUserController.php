01eee0d2c9cf0c3af0cae06e09d15139cbd096c7
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-08-18 20:58:04 -0300

    feat: - Controller painel de controle criado para as estatisticas

---
+++ app/Http/Controllers/DashboardUserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardUserController extends Controller
+{
+    public function statisticTaskUser(){
+        
+    }
+
+    public function statisticCategory(){
+
+    }
+}
