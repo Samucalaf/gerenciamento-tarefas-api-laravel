fc9a1fd8f98dc53973ab5a4ef8de419a6fd2013c
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-08-21 15:48:02 -0300

    refactor: estatisticasUsers, mostra quantos usuarios entraram no mes com cache para não sobrecarregar o banco

--- app/Http/Controllers/DashboardUserController.php
+++ app/Http/Controllers/DashboardUserController.php
@@ -6,6 +6,7 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Models\Task;
 use Illuminate\Support\Facades\Cache;
+use App\Models\User;
 
 class DashboardUserController extends Controller
 {
@@ -51,6 +52,26 @@ class DashboardUserController extends Controller
 
     public function statisticUser(Request $request)
     {
-        $user = $request->user();
+        $authUser = $request->user();
+        $requestUser = $request->all();
+
+
+
+        if (isset($requestUser['Users'])) {
+
+            $value1 = $requestUser['Users'][0];
+            $value2 = $requestUser['Users'][1];
+
+            $cacheKey = "users_users_{$value1}_{$value2}";
+
+
+            $users = Cache::remember($cacheKey, now()->addMinutes(5), function () use ($value1, $value2) {
+                return User::whereBetween('created_at', [$value1, $value2])
+                    ->get();
+            });
+
+
+            return response()->json($users);
+        }
     }
 }
