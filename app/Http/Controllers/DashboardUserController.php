ce2d56524f38ce9422c42f8bea69a25ef1a0e7f7
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-08-20 23:47:08 -0300

    feat: - implementação de estatistica por tarefas e categorias

--- app/Http/Controllers/DashboardUserController.php
+++ app/Http/Controllers/DashboardUserController.php
@@ -2,15 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Models\Task;
 
 class DashboardUserController extends Controller
 {
-    public function statisticTaskUser(){
-        
+    public function statisticTaskUser(Request $request)
+    {
+        $user = $request->user();
+        $requestUser = $request->all();
+
+
+        if (isset($requestUser['category'])) {
+            $category = Category::where('user_id', $user->id)
+                ->where('name', $requestUser['category'])
+                ->with('tasks')
+                ->get();
+
+            return response()->json($category);
+        }
+
+        if (isset($requestUser['amount'])) {
+            $allTasks = Task::where('user_id', $user->id)
+                ->get();
+
+            return response()->json($allTasks);
+        }
     }
 
-    public function statisticCategory(){
 
+    public function statisticCategory(Request $request)
+    {
+        $user = $request->user();
+
+        $requestUser = $request->all();
+
+        
+        if(isset($requestUser['categories'])){
+            $categories = Category::where('user_id', $user->id)
+            ->whereNull('deleted_at')
+            ->get();
+
+            return response()->json([
+                'tarefas criadas e ativas',
+                $categories
+            ]);
+        }
+
+
+    }
+
+    public function statisticUser(Request $request)
+    {
+        $user = $request->user();
     }
 }
