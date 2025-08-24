cb5be0014fe4ad09223f7560e1ba7e5d7a4e2003
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-08-24 17:58:48 -0300

    refactor: - Mudança na coluna name, agora ela é unica

--- database/migrations/2025_07_28_180229_create_categories_table.php
+++ database/migrations/2025_07_28_180229_create_categories_table.php
@@ -13,8 +13,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->index('name');
+            $table->string('name')->unique();
             $table->foreignId('user_id')->constrained();
             $table->string('description')->nullable();
             $table->softDeletes();
