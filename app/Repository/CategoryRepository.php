64793e2291bb2e4507a1f4fcefe9eec5998a6016
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-11-01 13:46:09 -0300

    feat: implement repository layer for categories

---
+++ app/Repository/CategoryRepository.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Repository;
+
+use App\Models\Category;
+
+class CategoryRepository
+{
+
+    protected $model;
+
+    public function __construct(Category $category)
+    {
+        $this->model = $category;
+    }
+
+    public function search(string $term)
+    {
+        return $this->model->where('name', 'LIKE', "%$term%")
+            ->orWhere('description', 'LIKE', "%$term%")
+            ->get();
+    }
+
+    public function allDeleted()
+    {
+        return $this->model->onlyTrashed()->with('name', 'asc')->get();
+    }
+    public function findAll()
+    {
+        return $this->model->with('tasks')->orderBy('name', 'asc')->get();
+    }
+
+    public function findById($id)
+    {
+        return $this->model->with('tasks')->find($id);
+    }
+
+    public function create($data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function update($id, $data)
+    {
+        $category = $this->model->find($id);
+        if ($category) {
+            $category->update($data);
+            return $category;
+        }
+        return null;
+    }
+
+    public function delete($id)
+    {
+        $category = $this->model->find($id);
+        if ($category) {
+            $category->delete();
+            return true;
+        }
+        return false;
+    }
+}
