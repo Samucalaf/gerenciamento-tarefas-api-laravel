2c2e9f8f24030d0d7724cc1246be3eb5e8b533a8
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-11-01 16:34:47 -0300

    feat: add tasks resource to control API responses

---
+++ app/Http/Resources/TasksResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+class TasksResource extends JsonResource
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'title' => $this->title,
+            'description' => $this->description,
+            'completed' => $this->completed,
+            'priority' => $this->priority,
+            'due_date' => $this->due_date,
+            'user_id' => $this->user_id,
+            'category_id' => $this->category_id,
+        ];
+    }
+}
